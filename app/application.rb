d77b7bd6384320cc15667be4b25484fda9340ddb
grant smith <grantsmith1256@gmail.com>
2019-02-01 23:37:16 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,27 @@
+require 'pry'
 class Application
+
+    @@items = []
+
   def call(env)
 
-    @@items = [Item.new(:name, :price)]
-
-    req = Rack::Request.new(env)
     resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
-    if req.path.match(/items/<Item>)
-
+    if req.path.match(/items/)
+      #binding.pry
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_name}
+      if item
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.status = 404
+      resp.write "Route not found"
+    end
 
     resp.finish
   end
